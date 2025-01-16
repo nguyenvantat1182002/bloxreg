1c934a5acc91eb15723fded5bc1f615820ed5d8c
Tat Nguyen Van <nguyenvantat1182002@gmail.com>
2025-01-16 12:32:46 +0700

    Update project files.

--- windows/main_window.py
+++ windows/main_window.py
@@ -2,7 +2,9 @@ import os
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtCore import QTimer
 from threads import AccountGeneratorThread
+from datetime import datetime
 
 
 class MainWindow(QMainWindow):
@@ -16,6 +18,9 @@ class MainWindow(QMainWindow):
         self.spinBox_3.valueChanged.connect(self.spinBox_3_valueChanged)
 
         self._account_generator = None
+        self._start_time = datetime.now()
+        self._title_update_timer = QTimer(self)
+        self._title_update_timer.timeout.connect(self._update_window_title)
 
     def spinBox_3_valueChanged(self, value: int):
         if self._account_generator:
@@ -35,14 +40,25 @@ class MainWindow(QMainWindow):
                 self._account_generator.timeout = self.spinBox_2.value()
                 self._account_generator.finished.connect(self._task_finished)
                 self._account_generator.start()
+
+                self._start_time = datetime.now()
+                self._title_update_timer.start(1000)
             case 'Dừng':
                 self.pushButton.setText('Dừng...')
                 self._account_generator.stop = True
+                self._title_update_timer.stop()
 
     def pushButton_2_clicked(self):
         path = os.path.join(os.getcwd(), 'output')
         os.makedirs(path, exist_ok=True)
         os.startfile(path)
+
+    def _update_window_title(self):
+        elapsed_time = datetime.now() - self._start_time
+        hours, remainder = divmod(elapsed_time.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        self.setWindowTitle(f'BLOX REG | {hours:02}:{minutes:02}:{seconds:02}')
 
     def _task_finished(self):
         self.pushButton.setText('Bắt đầu')
